87d011fc8f24516cecaf5e5c92fab96d4cfc83c0
ShibutElizabeth <ellis.shybut@mail.ru>
2024-09-14 17:43:18 +0200

    feat: smooth

--- src/App.js
+++ src/App.js
@@ -17,31 +17,36 @@ export const App = () => {
   const group = useRef();
   const bRef = useRef();
   const cRef = useRef();
-  const mref = useRef();
-
-
+  const aRef = useRef();
 
   function Rig() {
     let k = 2;
     let time = 0;
     
     useFrame((state, delta) => {
+      
+      // console.log(state.camera)
       // console.log(mref.current)
-      easing.damp3(
-        state.camera.position,
-        [Math.sin(-state.pointer.x) * 5, state.pointer.y * 3.5, 15 + Math.cos(state.pointer.x) * 10],
-        0.2,
-        delta,
-      );
+      // easing.damp3(
+      //   state.camera.position,
+      //   [Math.sin(-state.pointer.x) * 5, state.pointer.y * 3.5, 15 + Math.cos(state.pointer.x) * 10],
+      //   0.2,
+      //   delta,
+      // );
       state.camera.lookAt(0, 0, 0);
       if(cRef.current && bRef.current){
-        const epsilon = 0.01; // Допуск для точности
-        group.current.rotation.y -= 0.02;
+        const epsilon = 0.0; // Допуск для точности
+        group.current.rotation.y += 0.01;
         crystalRef.current.rotation.y -= 0.006;
-        crystalRef.current.rotation.x += 0.006;
+        easing.damp3(
+          crystalRef.current.rotation,
+          [Math.sin(-state.pointer.x), state.pointer.y, 0],
+          0.5,
+          delta
+        );
         const rotationY = group.current.rotation.y % (Math.PI * 2);
 
-        group.current.position.y = 0.5 * (Math.cos(time * k));
+        group.current.position.y = 0.7 * (Math.cos(time * k));
         time += delta;
 
         // Первый объект (виден от 0 до π/2 и от 3π/2 до 2π)
@@ -65,14 +70,16 @@ export const App = () => {
         position: [
             0, 0, 20
         ],
-        fov: 50
+        fov: 50,
+        // zoom: 50,
+        // isOrthographicCamera: true,
         }}>
         <color attach="background" args={["#e0e0e0"]}/>
         <spotLight position={[20, 20, 10]} penumbra={1} castShadow angle={0.2}/>
         {/* <Status position={[0, 0, -10]}/> */}
-        <Float floatIntensity={2}>
-          <Crystal crystalRef={crystalRef} groupRef={group} bRef={bRef} cRef={cRef} mref={mref}/>
-        </Float>
+        {/* <Float floatIntensity={2}> */}
+          <Crystal crystalRef={crystalRef} groupRef={group} bRef={bRef} cRef={cRef} aRef={aRef}/>
+        {/* </Float> */}
         <ContactShadows
             scale={100}
             position={[0, -8, 0]}
@@ -114,17 +121,9 @@ export const App = () => {
                 scale={[10, 50, 1]}
                 color={"rgb(23, 0, 92)"}
                 onUpdate={(self) => self.lookAt(0, 0, 0)}/>
-              {/* <Lightformer
-                ref={lightPink}
-                intensity={10}
-                scale={[10, 50, 1]}
-                position={[-4, 5, 0]}
-                color={"rgb(255, 0, 191)"}
-                onUpdate={(self) => self.lookAt(0, 0, 0)}/> */}
         </Environment>
         <Rig/>
       </Canvas> 
     </>
   );
 }
-//rgb(46, 210, 255) rgb(255, 0, 251) rgb(0, 13, 130) rgb(115, 0, 255)
--- src/Crystal.js
+++ src/Crystal.js
@@ -5,18 +5,18 @@ import {
 } from "@react-three/drei";
 import { Color } from "three";
 
-useGLTF.preload("/crypcoinPro.glb");
+useGLTF.preload("/crypcoinJ.glb");
 
 const Crystal = (props) => {
-    const { crystalRef, groupRef, bRef, cRef, mref } = props;
+    const { crystalRef, groupRef, bRef, cRef, aRef } = props;
     
-    const bc = useGLTF("/crypcoinPro.glb ");
+    const bc = useGLTF("/crypcoinJ.glb ");
     console.log(bc.nodes)
     return (
         <group name={"group"}>
             <mesh ref={crystalRef} name="crystal">
                 <icosahedronGeometry args={[6, 0]}/>
-                <MeshTransmissionMaterial ref={mref} backside backsideThickness={0.5} thickness={0.5} background={new Color('rgb(224, 224, 224)')}/>
+                <MeshTransmissionMaterial backside backsideThickness={0.5} thickness={0.5} background={new Color('rgb(224, 224, 224)')}/>
             </mesh>
             <group ref={groupRef}>
                 <group name={"bitcoin"}>
@@ -25,61 +25,80 @@ const Crystal = (props) => {
                     receiveShadow
                     castShadow
                     geometry={bc.nodes.Bitcoin.geometry}
-                    scale={2.5}
-                    // rotation={[0, Math.PI, 0]}
+                    scale={2.42}
+
+                    rotation={[0, Math.PI, 0]}
                     
                     >
-                        <MeshTransmissionMaterial 
+                        <meshBasicMaterial 
+                        roughness={1}
+                        color={"rgb(0, 0, 0)"}
+                        />
+                        {/* <MeshTransmissionMaterial 
                         backside 
                         backsideThickness={1} 
                         thickness={1}
                         transmissionSampler={true}
-                        distortion={0.5}
-                        distortionScale={1}
+                        // distortion={1}
+                        // distortionScale={1}
                         color={"rgb(100, 100, 100)"}
-                        />
+                        /> */}
                     </mesh>
                 </group>
                 <group ref={cRef} name={"crypgex"}>
-                    <mesh
-                    ref={cRef}
+                    {/* <mesh
+                    // ref={aRef}
                     receiveShadow
                     castShadow
                     geometry={bc.nodes.ArrowPart.geometry}
                     scale={2.5}
-                    // rotation={[0, Math.PI, 0]}
+                    // rotation={[0, Math.PI/2, 0]}
                     >
-                        <MeshTransmissionMaterial 
+                        <meshStandardMaterial
+                        // opacity={1}
+                        // transparent
+                        roughness={1}
+                        color={"rgb(100, 100, 100)"}
+                        />
+                        {/* <MeshTransmissionMaterial 
                         backside 
                         backsideThickness={0.5}
                         thickness={0.5}
                         transmissionSampler={true}
                         // roughness={0.5}
                         color={"rgb(255, 0, 255)"}
-                        distortion={0.5}
-                        distortionScale={2}
-                        />
-                    </mesh>
+                        // distortion={0.5}
+                        // distortionScale={2}
+                        /> 
+                    </mesh> */}
                     <mesh
+                    //  ref={cRef}
                     receiveShadow
                     castShadow
                     geometry={bc.nodes.XPart.geometry}
                     scale={2.5}
-                    // rotation={[0, -2*Math.PI, 0]}
+                    // rotation={[Math.PI/40, 0, 0]}
                     >
-                        <MeshTransmissionMaterial 
+                        <meshBasicMaterial 
+                        roughness={1}
+                        color={"rgb(0, 0, 0)"}
+                        />
+                        {/* <meshStandardMaterial
+                        roughness={1}
+                        color={"rgb(100, 100, 100)"}
+                        /> */}
+                        {/* <MeshTransmissionMaterial 
                         backside 
                         backsideThickness={1}
                         thickness={1}
                         transmissionSampler={true}
                         color={"rgb(100, 100, 100)"}
-                        distortion={0.5}
-                        distortionScale={1}
-                        />
+                        // distortion={1}
+                        // distortionScale={1}
+                        /> */}
                     </mesh>
                 </group>
             </group>
-            
         </group>
     )
 }
